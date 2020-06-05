c8ac0a24d80469f3de227044c48268ef4a75fb9c
ameen4455 <ameen4455@gmail.com>
2020-06-05 18:24:18 +0530

    added previous level

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import './Navbar.scss';
 import React, {useEffect, useState} from 'react';
 import { logout } from '../../auth0/auth0';
 import {get} from '../../auth0/http';
+import {useParams} from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faDoorClosed, faDoorOpen, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 import {API_ROOT} from '../../auth0/api_config';
 
 function Navbar(props) {
+    let {levelParam} = useParams();
     const [isOpen,setIsOpen] = useState(false);
     const toggleOpen = () => setIsOpen(!isOpen );
     const menuClass = `dropdown-menu${isOpen ? " show" : ""}`;
@@ -36,9 +40,19 @@ function Navbar(props) {
     },[])
     return(
         <nav className="navbar cursor-default nbar navbar-light d-flex">
+            <div>
             <a className="navbar-brand" href="/">
                 <img src={require('../../assets/images/logo.png')} className="d-inline-block align-top logo" alt="" />
             </a>
+            {levelParam &&
+            <a className="navbar-brand click-back pb-2" style={{marginLeft:'12vw'}} href="/game">
+                <div className={'d-flex ml-3 align-items-center'}>
+                    <FontAwesomeIcon icon={faArrowLeft} className="" />
+                    <div className={'ml-2'}> Go back to current level </div>
+                </div>
+            </a>
+            }
+            </div>
             <div id="user-info" className="d-flex">
                 <p className="score tex">My Score : <span style={{fontWeight:500}}>{user.score}</span></p>
                 <div className="d-flex">
