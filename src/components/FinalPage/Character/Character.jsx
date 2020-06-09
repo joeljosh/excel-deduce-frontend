d81e849954f80b6583d3eaaf990bbab2e3b27152
ameen4455 <ameen4455@gmail.com>
2020-06-09 23:40:51 +0530

    added background favicon

--- src/components/FinalPage/Character/Character.jsx
+++ src/components/FinalPage/Character/Character.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Character.scss';
 import rick from '../../../assets/images/rick.png';
 import man from '../../../assets/images/man.png';
+import manPeace from '../../../assets/images/man-front.png';
+
 
 function Character(props){
+
+    const [isFront,setIsfront] = useState(false);
+
+    useEffect(()=>{
+        if(props.start){
+            setTimeout( ()=>{
+                setIsfront(true)
+            },5000)
+        }
+    },[props.start]);
+
     return(
         props.start &&
         <div className="char-box">
             <img src={rick} className="portal" alt="" />
-            <img src={man} className="man" alt="" />
+            <img src={isFront? manPeace :man} className="man" alt="" />
         </div>
     )
 }
