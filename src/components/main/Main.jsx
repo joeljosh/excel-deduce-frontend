21813c4da439d6386d245803a4f756725fef6efb
AFNAN NAVAZ <afnan@pop-os.localdomain>
2020-06-10 13:33:03 +0530

    hints

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -50,6 +50,11 @@ function Main(props) {
     }
   };
 
+  const is_url = (str) =>{
+    const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    return regexp.test(str);
+}
+
   const fsize = (num) => {
     if(num >= 150){
       return 1.5;
@@ -238,6 +243,8 @@ function Main(props) {
                       className="bubble cursor-pointer"
                     >
                       {level.hints.map((x, i) => (
+                        is_url(x.hint) ? 
+                        <p key={i}><a href={x.hint}>hint : {i}</a></p> : 
                         <p key={i}>{x.hint}</p>
                       ))}
                     </div>
