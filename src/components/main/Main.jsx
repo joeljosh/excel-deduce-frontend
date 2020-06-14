d3cfdd32d5568c5ecd34d9c824e65cad39fbbec2
AFNAN NAVAZ <afnan@pop-os.localdomain>
2020-06-14 21:43:26 +0530

    fix

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -246,8 +246,8 @@ function Main(props) {
                     >
                       {level.hints.map((x, i) => (
                         is_url(x.hint) ? 
-                        <p key={i}><a href={x.hint}>hint : {i}</a></p> : 
-                        <p key={i}>{x.hint}</p>
+                        <p key={i}><li><a href={x.hint}>hint : {i+1}</a></li></p> : 
+                        <p key={i}><li>{x.hint}</li></p>
                       ))}
                     </div>
                   ) : (
