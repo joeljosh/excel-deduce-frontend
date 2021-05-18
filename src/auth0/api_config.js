339b2a9a7d22a6989f2bf5e39e5e688ba0ed84a1
Afnan-Navaz <afnannavaz@gmail.com>
2021-05-18 12:04:04 +0530

    added auth0

--- src/auth0/api_config.js
+++ src/auth0/api_config.js
@@ -1,3 +1,6 @@
- export const API_ROOT = "https://deduce-backend-q7no2lk4qa-de.a.run.app/api/"
+ export const API_ROOT = "https://deducebackend-ovhv32pszq-uc.a.run.app/api/"
+ 
+//  "https://deduce-backend-q7no2lk4qa-de.a.run.app/api/"
+ 
 // export const API_ROOT = "http://35.200.223.244/api/"
-//export const API_ROOT = "http://localhost:8000/api/";
+// export const API_ROOT = "http://localhost:8000/api/";
